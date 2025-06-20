c3e566c294c0c97e59dda9f0c424062dfa885722
Cahyo Budi Aditya <74086011+cabudit@users.noreply.github.com>
2025-06-20 13:56:05 +0700

    remove secrets.h from repo

--- include/secrets.h
+++
@@ -1,9 +0,0 @@
-// secrets.h
-#ifndef SECRETS_H
-#define SECRETS_H
-
-const char* WIFI_SSID = "mboed";
-const char* WIFI_PASSWORD = "mboed123";
-const char* TELEGRAM_BOT_TOKEN = "7987528842:AAHcFAczdYPcx1qGEgrJ9hmi2qppk8GM1_o";
-
-#endif
